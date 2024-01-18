696898db46e48c3e0d4fa7cc2271c22c6e8a5783
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2024-01-18 07:39:11 +0000

    check test cases

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,17 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  
+  const weather= {temperature:25, conditions: "Sunny"};
+  const tempColor= weather.temperature>20 ? "red": "blue";
+  // console.log(tempColor);
+  
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Weather Report</h1>
+        <p>Temperature: <span style={{color:tempColor}}>{weather.temperature}</span></p>
+        <p>Conditions: <span>{weather.conditions}</span></p>
     </div>
   )
 }
